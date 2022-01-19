0e07c494b7ea72197950c116262d455e39547fb2
Sagal Ghelle <sagal.ghelle@gmail.com>
2022-01-18 20:25:52 -0500

    added navbar and form input field

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import InputForm from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
